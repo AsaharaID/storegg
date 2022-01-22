aee5767b3a94fee6fc71db30c92ad212a5d91f74
Syah Reza <muhamadsyahr45@gmail.com>
2022-01-22 15:30:11 +0700

    commit

--- components/organisms/navbar/index.tsx
+++ components/organisms/navbar/index.tsx
@@ -1,6 +1,8 @@
 /** @format */
 import Image from "next/image";
+import Auth from "./auth";
 import Menu from "./menu";
+import ToogleMenu from "./toogleMenu";
 export default function Navbar() {
 	return (
 		<section>
@@ -9,17 +11,7 @@ export default function Navbar() {
 					<a className="navbar-brand" href="#">
 						<Image src="/icon/logo.svg" width={60} height={60} />
 					</a>
-					<button
-						className="navbar-toggler"
-						type="button"
-						data-bs-toggle="collapse"
-						data-bs-target="#navbarNav"
-						aria-controls="navbarNav"
-						aria-expanded="false"
-						aria-label="Toggle navigation"
-					>
-						<span className="navbar-toggler-icon"></span>
-					</button>
+					<ToogleMenu />
 					<div className="collapse navbar-collapse" id="navbarNav">
 						<ul className="navbar-nav ms-auto text-lg gap-lg-0 gap-2">
 							<Menu title="Home" active href="/" />
@@ -27,15 +19,7 @@ export default function Navbar() {
 							<Menu title="Rewards" href="/" />
 							<Menu title="Discover" href="/" />
 							<Menu title="Global Rank" href="/" />
-							<li className="nav-item my-auto">
-								<a
-									className="btn btn-sign-in d-flex justify-content-center ms-lg-2 rounded-pill"
-									href="./src/sign-in.html"
-									role="button"
-								>
-									Sign In
-								</a>
-							</li>
+							<Auth isLogin />
 						</ul>
 					</div>
 				</div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,6 +28,7 @@ import "../styles/transactions.css";
 import "../styles/transactions-detail.css";
 import "../styles/edit-profile.css";
 import "../styles/complete-checkout.css";
+import "../styles/navbar-log-in.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
