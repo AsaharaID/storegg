2c1e5f2f84a8139eb87dddf3102f9903543e9d12
Syah Reza <muhamadsyahr45@gmail.com>
2022-02-02 09:43:48 +0700

    commit

---
+++ components/TransactionsDetailContent/row.tsx
@@ -0,0 +1,16 @@
+/** @format */
+
+interface RowProps{
+    label: string;
+    value: String | number;
+    className?: string;
+}
+export default function Row(props : Partial<RowProps>) {
+	const { label, value, className } = props;
+	return (
+		<p className="text-lg color-palette-1 mb-20">
+			{label}
+			<span className={`purchase-details ${className}`}>{value}</span>
+		</p>
+	);
+}
